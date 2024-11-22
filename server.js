9b4fedf4a51b00f624427e94266866a5b8b858d3
Francisco J <franlsym@users.noreply.github.com>
2024-11-22 17:40:26 +0000

    Refactor server.js to remove comments and streamline static file serving

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-// server.js
-// where your node app starts
-
 // init project
 var express = require('express');
 var app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
